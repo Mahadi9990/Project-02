2d3d5f0ce8eb4b3cff5b2c7ef0a1d227a1dbf76b
Mahadi9990 <mdshafayat9999@gmail.com>
2024-03-20 22:03:00 +0600

    add BrowserRouter,Routes,Route

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,19 @@
-import React from 'react'
-
+import {BrowserRouter,Routes,Route} from 'react-router-dom';
+import Home from './pages/Home';
+import About from './pages/About';
+import Singin from './pages/Singin';
+import Singup from './pages/Singup';
+import Profile from './pages/Profile';
 export default function App() {
   return (
-    <div>App</div>
+    <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Home/>}/>
+      <Route path="about" element={<About/>}/>
+      <Route path="sing-in" element={<Singin/>}/>
+      <Route path="sing-up" element={<Singup/>}/>
+      <Route path="Profile" element={<Profile/>}/>
+    </Routes>
+    </BrowserRouter>
   )
 }

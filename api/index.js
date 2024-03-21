c2ec0b06b8d2cf9657219861d80be7e064c59ec4
Mahadi9990 <mdshafayat9999@gmail.com>
2024-03-21 11:50:02 +0600

    Create sarver

---
+++ api/index.js
@@ -0,0 +1,8 @@
+import express from 'express'
+import mongoose from 'mongoose'
+mongoose.connect()
+const app =express()
+
+app.listen(3000,()=>{
+    console.log("Server is runing on port 3000")
+})

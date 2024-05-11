7c8737633b0f1a631b1286f2c5ffc114271fb754
Mahadi9990 <mdshafayat9999@gmail.com>
2024-05-11 23:30:09 +0600

    complet About page

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -2,6 +2,9 @@ import React from 'react'
 
 export default function About() {
   return (
-    <div>About</div>
+    <div>
+      <h1 className='text-center text-3xl font-semibold'>About page</h1>
+      <p className='p-5'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis quam quae dolor voluptas eveniet eius magni, dicta voluptatum assumenda? Quaerat quas ipsum consequatur tempora, ratione et eum cupiditate cumque, doloribus provident blanditiis, laboriosam consequuntur iusto obcaecati. Ad ipsam odit eum harum excepturi officiis animi illo necessitatibus fugit facilis perspiciatis, nesciunt aperiam sit fuga! Rem maiores molestias harum, doloribus ut velit eligendi dolorum minima cupiditate a laboriosam nihil ab ad molestiae? Iste atque nesciunt et, dicta dolorum nostrum quis eligendi earum tempore porro optio eveniet dolorem quae culpa minima odit, voluptatum beatae praesentium neque quaerat veniam sed non? Porro, voluptatibus quo?</p>
+    </div>
   )
 }
